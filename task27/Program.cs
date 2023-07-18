19b8ffbd999487a6efebe1f2233dafe6fc07abe9
alexeicaps <caps.alex@mail.ru>
2023-07-18 06:18:04 +0300

    Resolved Task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -2,3 +2,24 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int DrawNumbers(int value)
+{
+    int result = 0;
+    while(value > 0)
+    {
+        result = result + value % 10;
+        value = value / 10;
+    }
+    return result;
+}
+
+int N = Prompt("Введите число: ");
+Console.WriteLine($"Сумма цифр числа {N} -> " + DrawNumbers(N));
