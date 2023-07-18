e8a9c04dbf01d6be65c3c354c1d184f27589f24a
Alexei Capsamun <caps.alex@mail.ru>
2023-07-18 10:13:28 +0300

    Updated tasks

--- task27/Program.cs
+++ task27/Program.cs
@@ -2,3 +2,31 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+
+// int SymbolCountNumber(int num1);
+// {
+//     int count = 0;
+//     while (num1 > 0)
+//     {
+//         num1 = num1 / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// void SumSymbolNumber(int a, int len)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= len; i++)
+//     {
+
+//     }
+// }
